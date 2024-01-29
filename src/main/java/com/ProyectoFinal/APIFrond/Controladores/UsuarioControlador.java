d1a6917028bf86306289ff63ecddc953d7a37d29
JosePcClase <josemanuelmorenoguerra96@gmail.com>
2024-01-29 14:15:16 +0100

    Implementacion Servicios

--- src/main/java/com/ProyectoFinal/APIFrond/Controladores/UsuarioControlador.java
+++ src/main/java/com/ProyectoFinal/APIFrond/Controladores/UsuarioControlador.java
@@ -3,6 +3,7 @@ package com.ProyectoFinal.APIFrond.Controladores;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.ProyectoFinal.APIFrond.Modelo.UsuarioDAO;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioControlador {
 	
 	@Autowired
@@ -24,7 +26,7 @@ public class UsuarioControlador {
 		return this.repositorio.findAll();
 	}
 	
-	@PostMapping("/nuevousuario")
+	@PostMapping("/registrar")
 	public UsuarioDAO crearUsuario(@RequestBody UsuarioDAO nuevoUsuario) {
 	    return repositorio.save(nuevoUsuario);
 	}
---
+++ src/main/java/com/ProyectoFinal/APIFrond/Servicios/UsuarioServicios.java
@@ -0,0 +1,8 @@
+package com.ProyectoFinal.APIFrond.Servicios;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServicios {
+
+}
